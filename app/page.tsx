5666407e8eb09c125bc567c55403c7d367df1700
unknown <ejloudalec13@gmail.com>
2026-05-04 16:35:29 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -112,13 +112,16 @@ export default function Home() {
     try {
       setLoading(true);
 
-      const response = await fetch("http://127.0.0.1:8000/predict", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ image }),
-      });
+      const response = await fetch(
+  `${process.env.NEXT_PUBLIC_API_URL}/predict`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ image }),
+  }
+);
 
       const data = await response.json();
 
